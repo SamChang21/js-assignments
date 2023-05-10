f8e07bfe4470df173bae457ca246b021feb1bf92
SamChang21 <samchang@pacificamerican.org>
2023-05-10 10:47:57 +0800

    Update L13.js

--- Lessons/Lesson13/L13.js
+++ Lessons/Lesson13/L13.js
@@ -4,43 +4,40 @@ const song3 = new Audio('audio/03.mp3')
 
 function playSong(song){
     if (song === 1) {
-    console.log('play')
     song1.play()
+    song2.pause()
+    song3.pause()
     }
     if (song === 2) {
-        console.log('play')
-        song2.play()
+    song2.play()
+    song1.pause()
+    song3.pause()
     }
     if (song === 3) {
-        console.log('play')
-        song3.play()
+    song3.play()
+    song2.pause()
+    song1.pause()
     }
 }
 function stopSong(song){
     if (song === 1) {
-        console.log('play')
         song1.pause()
     }
     if (song === 2) {
-        console.log('play')
         song2.pause()
     }
     if (song === 3) {
-        console.log('play')
         song3.pause()
     }
 }
 function restartSong(song){
     if (song === 1) {
-        console.log('play')
         song1.pause()
     }
     if (song === 2) {
-        console.log('play')
         song2.pause()
     }
     if (song === 3) {
-        console.log('play')
         song3.pause()
     }
 }
